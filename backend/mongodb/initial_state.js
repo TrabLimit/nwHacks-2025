178648148d7edbc835707a7884be2615d759c72b
JacobZhu-zjc <jacob-zhu@outlook.com>
2025-01-18 23:53:59 -0800

    Initial working backend implementation

---
+++ backend/mongodb/initial_state.js
@@ -0,0 +1,14 @@
+const initialState = `[
+    {
+        "_id": "79bc2e0e-13f2-4873-bae1-c1c9a5de5cff",
+        "name": "Squats",
+        "description": "The squat is a lower body exercise. It is a functional exercise that can help you in your daily activities and keep you strong.",
+        "muscleGroups": ["Quadriceps", "Hamstrings", "Glutes"],
+        "exercises": ["Squats"],
+        "difficulty": "Intermediate",
+        "equipment": ["Barbell"],
+        "music": "Rock"
+    }
+]`
+
+module.exports = { initialState };
---
+++ backend/routes/api.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const {v4: uuidv4} = require('uuid');
+const router = express.Router();
+const userModel = require("../mongodb/schemas").userModel;
+const workoutModel = require("../mongodb/schemas").workoutModel;
+
+// GET endpoint to fetch all data for a specific user
+router.get("/users/:uuid", async (req, res) => {
+    try {
+        // Getting and responding with user data
+        const users = await userModel.find({_id: req.params.uuid});
+        res.send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// POST endpoint to create a new user
+router.post("/users", async (req, res) => {
+    try {
+        // Creating a new user
+        const newUser = new userModel({
+            _id: uuidv4(),
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            muscleGroupFocus: req.body.muscleGroupFocus,
+            preferredMusic: req.body.preferredMusic,
+        });
+        // Saving the user to the database
+        await newUser.save();
+        res.send(newUser);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// PUT endpoint to update a user's data
+router.put("/users/:uuid", async (req, res) => {
+    try {
+        // Updating the user's data with the fields provided in the request body
+        await userModel.updateOne({_id: req.params.uuid}, req.body);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// GET endpoint to fetch all workouts
+router.get("/workouts", async (req, res) => {
+    try {
+        // Getting and responding with all workouts
+        const workouts = await workoutModel.find({});
+        res.send(workouts);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+module.exports = router;
